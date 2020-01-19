ea5bc14b9b06483873d90f56a596e8877e1a6929
Arjun <denverbdr@protonmail.com>
2020-01-19 12:20:56 -0800

    Fix build/test for 2.1.1

--- src/gtest/test_keys.cpp
+++ src/gtest/test_keys.cpp
@@ -7,39 +7,19 @@
 
 #include <gtest/gtest.h>
 
-
-class EncodeKeysTestFixture : public ::testing::TestWithParam<std::tuple<CBaseChainParams::Network, std::tuple<string, string, string>>>
+TEST(Keys, EncodeAndDecodeSapling)
 {
-    public:
-        EncodeKeysTestFixture()
-        {
-            
-            SelectParams(std::get<0>(GetParam()));
-        
-            std::tuple<string, string, string> keys_for_network = std::get<1>(GetParam());
-            sapling_extended_spend_key = std::get<0>(keys_for_network);
-            sapling_payment_address = std::get<1>(keys_for_network);
-            sapling_incoming_viewing_key = std::get<2>(keys_for_network);
-            master_sapling_sk = GetTestMasterSaplingSpendingKey();
-        }
+    SelectParams(CBaseChainParams::MAIN);
 
-    protected:
-        std::string sapling_extended_spend_key;
-        std::string sapling_payment_address;
-        std::string sapling_incoming_viewing_key;
-        libzcash::SaplingExtendedSpendingKey master_sapling_sk;
+    auto m = GetTestMasterSaplingSpendingKey();
 
-};
-
-TEST_P(EncodeKeysTestFixture, EncodeAndDecodeSapling)
-{
     for (uint32_t i = 0; i < 1000; i++) {
-        auto sk = master_sapling_sk.Derive(i);
+        auto sk = m.Derive(i);
         {
             std::string sk_string = EncodeSpendingKey(sk);
             EXPECT_EQ(
-                sk_string.substr(0, sapling_extended_spend_key.length()),
-                sapling_extended_spend_key);
+                sk_string.substr(0, 24),
+                Params().Bech32HRP(CChainParams::SAPLING_EXTENDED_SPEND_KEY));
 
             auto spendingkey2 = DecodeSpendingKey(sk_string);
             EXPECT_TRUE(IsValidSpendingKey(spendingkey2));
@@ -53,8 +33,8 @@ TEST_P(EncodeKeysTestFixture, EncodeAndDecodeSapling)
 
             std::string addr_string = EncodePaymentAddress(addr);
             EXPECT_EQ(
-                addr_string.substr(0, sapling_payment_address.length()),
-                sapling_payment_address);
+                addr_string.substr(0, 2),
+                Params().Bech32HRP(CChainParams::SAPLING_PAYMENT_ADDRESS));
 
             auto paymentaddr2 = DecodePaymentAddress(addr_string);
             EXPECT_TRUE(IsValidPaymentAddress(paymentaddr2));
@@ -63,33 +43,5 @@ TEST_P(EncodeKeysTestFixture, EncodeAndDecodeSapling)
             auto addr2 = boost::get<libzcash::SaplingPaymentAddress>(paymentaddr2);
             EXPECT_EQ(addr, addr2);
         }
-        {
-            auto ivk = sk.ToXFVK().fvk.in_viewing_key();
-            std::string ivk_string = EncodeViewingKey(ivk);
-            EXPECT_EQ(
-                ivk_string.substr(0, sapling_incoming_viewing_key.length()),
-                sapling_incoming_viewing_key);
-
-            auto viewing_key = DecodeViewingKey(ivk_string);
-            EXPECT_TRUE(IsValidViewingKey(viewing_key));
-
-            auto ivk2 = boost::get<libzcash::SaplingIncomingViewingKey>(&viewing_key);
-            ASSERT_TRUE(ivk2 != nullptr);
-            EXPECT_EQ(*ivk2, ivk);
-        }
     }
 }
-
-INSTANTIATE_TEST_CASE_P(
-    Keys,
-    EncodeKeysTestFixture,
-    ::testing::Values(
-        std::make_tuple(    
-            CBaseChainParams::MAIN, 
-            std::make_tuple("secret-extended-key-main", "ys", "zivks")),
-        std::make_tuple(    
-            CBaseChainParams::TESTNET, 
-            std::make_tuple("secret-extended-key-test", "ytestsapling", "zivktestsapling")),
-        std::make_tuple(    
-            CBaseChainParams::REGTEST, 
-            std::make_tuple("secret-extended-key-regtest", "yregtestsapling", "zivkregtestsapling"))));
