44a899fa70dca2d14a1fad06c0df984fa43854bc
Arjun S <denverbdr@protonmail.com>
2020-09-10 10:15:57 -0700

    Update depreciation to Nov 2021

--- src/deprecation.h
+++ src/deprecation.h
@@ -8,8 +8,8 @@
 // Deprecation policy:
 // * Shut down 16 weeks' worth of blocks after the estimated release block height.
 // * A warning is shown during the 2 weeks' worth of blocks prior to shut down.
-static const int APPROX_RELEASE_HEIGHT = 700000;
-static const int WEEKS_UNTIL_DEPRECATION = 34;
+static const int APPROX_RELEASE_HEIGHT = 810000;
+static const int WEEKS_UNTIL_DEPRECATION = 60;
 static const int DEPRECATION_HEIGHT = APPROX_RELEASE_HEIGHT + (WEEKS_UNTIL_DEPRECATION * 7 * 24 * 24);
 
 // Number of blocks before deprecation to warn users
